bab26bd3a45a2882aa27821a1120c29a1c66f206
massu0819 <massu4869@gmail.com>
2021-10-17 14:55:59 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a8ccf357394a3c41cf1523c755d5433d0a1f7fc310acbab2545718f047e92d88eb59e6894fc2fb1de0f7f30b8622169997d29d0047b96dafd44fc71f141cd13c'
